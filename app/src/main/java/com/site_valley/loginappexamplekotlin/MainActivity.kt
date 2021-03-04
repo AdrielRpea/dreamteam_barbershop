65e5abf55b60ae8e1b68b773ae91022afc8e1405
Abhinav <xabhi7549@gmail.com>
2021-03-04 15:53:52 +0530

    Initial commit

---
+++ app/src/main/java/com/site_valley/loginappexamplekotlin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.site_valley.loginappexamplekotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onclick(view: View) {
+        login(
+            findViewById<TextInputEditText>(R.id.edtUserName).text.toString(),
+            findViewById<TextInputEditText>(R.id.edtPassword).text.toString()
+        )
+    }
+
+    fun login(userName: String, password: String) {
+        if (userName == "name" && password == "1234") {
+            startActivity(Intent(this, HomeActivity::class.java))
+            Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
